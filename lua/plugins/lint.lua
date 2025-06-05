ae09b99d603c0e4f2c528f7726c50234efa9870a
chmtomasz <chmtomasz@gmail.com>
2025-06-05 18:26:44 +0300

    reformated structure

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,23 @@
+return {
+  { -- Linting
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        markdown = { 'markdownlint' },
+      }
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          -- Only run the linter in buffers that you can modify in order to
+          -- avoid linting issues in read-only files or files without write access.
+          if vim.bo.modifiable and not vim.bo.readonly then
+            lint.try_lint()
+          end
+        end,
+      })
+    end,
+  },
+}
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,1 +1,37 @@
-return { 'nvim-tree/nvim-tree.lua' }
+return {
+  'nvim-treek/nvim-tree.lua',
+  opts = {
+    disable_netrw = true,
+    hijack_netrw = true,
+    view = {
+      side = 'left',
+      width = 30,
+    },
+    actions = {
+      open_file = {
+        resize_window = true,
+      },
+    },
+    renderer = {
+      icons = {
+        glyphs = {
+          default   = "",
+          symlink   = "",
+          folder    = {
+            arrow_open   = "",
+            arrow_closed = "",
+            default      = "",
+            open         = "",
+          },
+          git = {
+            unstaged  = "✗",
+            staged    = "✓",
+            unmerged  = "",
+            renamed   = "➜",
+            untracked = "★",
+          },
+        },
+      },
+    },
+  },
+}
